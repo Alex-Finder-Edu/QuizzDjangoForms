44b7fb39ea59de194370e7985899537eace3c8b4
Alex Finder <alex.finder.edu@gmail.com>
2026-05-03 18:06:22 +0300

    Added models for Questions Subdomains and Domains

--- quizz_web_app/admin.py
+++ quizz_web_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Domain, Subdomain, Question
 # Register your models here.
+
+admin.site.register(Domain)
+admin.site.register(Subdomain)
+admin.site.register(Question)
--- quizz_web_app/models.py
+++ quizz_web_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Domain(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Subdomain(models.Model):
+    name = models.CharField(max_length=100)
+    domain = models.ForeignKey(Domain, on_delete=models.CASCADE, related_name="subdomains")
+
+    class Meta:
+        unique_together = ("name", "domain")
+
+    def __str__(self):
+        return f"{self.domain} — {self.name}"
+
+
+class Question(models.Model):
+    text = models.TextField()
+    correct_answer = models.TextField()
+    subdomain = models.ForeignKey(Subdomain, on_delete=models.CASCADE, related_name="questions")
+
+    def __str__(self):
+        return self.text[:80]
