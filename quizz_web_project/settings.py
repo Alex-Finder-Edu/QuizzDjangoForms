5af853be293167be1ab63af793d10b96e9911a72
Alex Finder <alex.finder.edu@gmail.com>
2026-06-02 13:49:38 +0300

    Fix Edit and Delete buttons

--- quizz_web_project/settings.py
+++ quizz_web_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k=j$rz6tfpbck_d*cqf@!gb3iigk+)qpu)!lq7kqu7q&86zq_7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.130', 'localhost']
 
 
 # Application definition
